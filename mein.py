66516e09ef1489410b8d2ad99ecaca6ac9589884
4yDo-i <illya15996369@gmail.com>
2022-11-20 17:13:10 +0200

    finished work on voting

--- mein.py
+++ mein.py
@@ -5,7 +5,7 @@ bot = telebot.TeleBot('5657149485:AAEthHetvX404j7xqdOLdq3gZIYErCkr3rE')
 
 poll_items = []  # ['text' - (name) , 0 - (scores)]
 id_items = [0]  # [id - id користувачів]
-message_id_list = []
+message_id_list = {}
 
 
 @bot.message_handler(commands=['start'])
@@ -23,6 +23,7 @@ def add_id(message):
     if (bababoi == 0):
         bot.send_message(message.chat.id, f"Your ID has been added to the list", parse_mode='html')
         id_items.append(message.chat.id)
+        message_id_list.update({message.chat.id: []})
         bot.send_message(message.chat.id, f"{id_items}", parse_mode='html')
 
 
@@ -33,6 +34,19 @@ def vote(message):
     button_2 = types.InlineKeyboardButton("Vote", callback_data='send')
     markup.add(button_1, button_2)
     bot.send_message(message.chat.id, f"Pls send first message", parse_mode='html', reply_markup=markup)
+
+@bot.message_handler(commands=['top'])
+def top(message):
+    array = poll_items
+    for j in range(1, 3):
+        i = 0
+        a = 0
+        while i <= len(array):
+             if int(array[i][1]) >= a:
+                 a = int(array[i][1])
+        bot.send_message(message.chat.id, f"{j}) {array[a][0]} — {array[a][1]}", parse_mode='html')
+        array.pop(a)
+
 
 
 @bot.message_handler(commands=['show'])
@@ -61,26 +75,25 @@ def callback(call):
                     but_2 = types.InlineKeyboardButton("2", callback_data=f'2_{poll_item}')
                     but_3 = types.InlineKeyboardButton("3", callback_data=f'3_{poll_item}')
                     markup.add(but_0, but_1, but_2, but_3)
-                    bot.send_message(id_items[id_], f'{name} — {score}', parse_mode='html', reply_markup=markup)
-                    #message_id_list.append(bot.send_message(id_items[id_], f'{name} - {score}', parse_mode='html',reply_markup=markup).message_id)
+                    mess_id = bot.send_message(id_items[id_], f'{name}', parse_mode='html',
+                                               reply_markup=markup).message_id
+                    message_id_list[id_items[id_]].append(mess_id)
 
-
-@bot.callback_query_handler(func=lambda call: True)
-def callback(call):
-    global poll_items
-    if call.message:
-        bot.send_message(call.message.id, f'Gau', parse_mode='html')
+        id_ = call.message.chat.id
         for i in range(len(poll_items)):
             if call.data == f'0_{i}':
-                pass
+                bot.delete_message(id_, message_id_list[id_][i])
+
             elif call.data == f'1_{i}':
-                pass
+                bot.delete_message(id_, message_id_list[id_][i])
                 poll_items[i][1] = poll_items[i][1] + 1
+
             elif call.data == f'2_{i}':
-                pass
+                bot.delete_message(id_, message_id_list[id_][i])
                 poll_items[i][1] = poll_items[i][1] + 2
+
             elif call.data == f'3_{i}':
-                pass
+                bot.delete_message(id_, message_id_list[id_][i])
                 poll_items[i][1] = poll_items[i][1] + 3
 
 
